149660aba8b1ac61ca13da7374a5329eb77af523
Marcus Vinicius <vmvini@hotmail.com>
2016-05-07 20:41:26 -0300

    metodo para validar token e retornar email do usuario

--- src/main/java/ifpb/pod/proj/sessiontoken/SessionToken.java
+++ src/main/java/ifpb/pod/proj/sessiontoken/SessionToken.java
@@ -4,6 +4,7 @@ package ifpb.pod.proj.sessiontoken;
 import io.jsonwebtoken.JwtBuilder;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.SignatureException;
 
 import javax.crypto.spec.SecretKeySpec;
 import java.io.UnsupportedEncodingException;
@@ -15,20 +16,20 @@ import java.security.Key;
 public class SessionToken {
 
 
-    private static String getSecretKey(){
-        return "201u30i2j1sok21mslkmlisua9uw103j21knkns.,M,X.NJHUWHQIWU2HK3J1NS09JOKNXSXKMSANDKJOWIEJ12O";
+    private static Key getSecretKey() throws UnsupportedEncodingException{
+
+            String raw = "201u30i2j1sok21mslkmlisua9uw103j21knkns.,M,X.NJHUWHQIWU2HK3J1NS09JOKNXSXKMSANDKJOWIEJ12O";
+            byte[] apiKeySecretBytes = raw.getBytes("UTF-8");
+            Key key = new SecretKeySpec(apiKeySecretBytes, SignatureAlgorithm.HS256.getJcaName());
+
+            return key;
     }
 
     public static String generateToken(String userEmail){
         try {
-            byte[] apiKeySecretBytes = getSecretKey().getBytes("UTF-8");
-
-            Key key = new SecretKeySpec(apiKeySecretBytes, SignatureAlgorithm.HS256.getJcaName());
-
-
             JwtBuilder builder = Jwts.builder()
                     .setSubject(userEmail) //email para identificar o usuario
-                    .signWith(SignatureAlgorithm.HS256, key);
+                    .signWith(SignatureAlgorithm.HS256, getSecretKey());
 
              String token = builder.compact();
             return token;
@@ -39,9 +40,21 @@ public class SessionToken {
 
     }
 
+
     //METODO PARA DECODIFICAR O TOKEN E RETORNAR O EMAIL QUE ELE POSSUI, EM CASO DE TOKEN VÁLIDO.
-    public String getEmailFromToken(){
-        return null;
+    public static String getEmailFromToken(String token){
+
+        try {
+            String userEmail = Jwts.parser().setSigningKey(getSecretKey()).parseClaimsJws(token).getBody().getSubject();
+            return userEmail;
+        }
+        catch(UnsupportedEncodingException e){
+            return null;
+        }
+        catch(SignatureException e){
+            return null;
+        }
+
     }
 
 
